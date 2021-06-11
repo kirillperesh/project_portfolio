b76604b59d5ed710d19be891151e860ef464bfae
kirill <kirill.fasmer@gmail.com>
2021-06-11 10:19:31 +0300

    update models and tests

--- glyke_back/apps.py
+++ glyke_back/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class GlykeBackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'glyke_back'
+
+    def ready(self):
+        import glyke_back.signals
+
