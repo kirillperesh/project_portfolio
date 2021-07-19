fa8317ff695d983eb9ad848a15fb9cdfef45b4e3
kirill <kirill.fasmer@gmail.com>
2021-07-19 19:28:09 +0300

    update products_staff

--- glyke_back/static/glyke_back/js/products_staff_datatables.js
+++ glyke_back/static/glyke_back/js/products_staff_datatables.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#products_staff_table').DataTable( {
         "pagingType": "full_numbers",
         scrollX: true,
+        "lengthMenu": [ [25, 50, 100, -1], [25, 50, 100, "All"]],
 
     } );
 } );
