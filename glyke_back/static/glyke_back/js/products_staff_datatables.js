5f01fb543306c7e62f9cd1dfdf2710c70a02d368
kirill <kirill.fasmer@gmail.com>
2021-07-20 19:26:17 +0300

    update products_staff

--- glyke_back/static/glyke_back/js/products_staff_datatables.js
+++ glyke_back/static/glyke_back/js/products_staff_datatables.js
@@ -1,8 +1,35 @@
 $(document).ready(function() {
     $('#products_staff_table').DataTable( {
+        dom: 'Blfrtip',
         "pagingType": "full_numbers",
         scrollX: true,
         "lengthMenu": [ [25, 50, 100, -1], [25, 50, 100, "All"]],
+        buttons: [
+            'colvis'
+        ],
+
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select style="width: 100%;" class="col-filter"><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        }
+
 
     } );
+
 } );
