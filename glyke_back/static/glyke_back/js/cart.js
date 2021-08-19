3ea0be45503c1b4fe292185e647c5b90315e8760
kirill <kirill.fasmer@gmail.com>
2021-08-19 12:09:29 +0300

    update cart page

--- glyke_back/static/glyke_back/js/cart.js
+++ glyke_back/static/glyke_back/js/cart.js
@@ -50,13 +50,13 @@ $(document).ready(function() {
         stock = $(this).parent().parent().children(".product-stock").children(".stock-number")[0].innerHTML; // number of products available
         if (current_value > stock) {
             event.target.value = stock
-            var myModal = new jBox('Modal', {
-                title: "Sorry, we're not able to ship more of this product yet",
-                content: "Only " + stock + " items available",
-                animation: 'zoomIn',
-                addClass: 'alert_no_stock',
+            var out_of_stock_popup = new jBox('Modal', {
+              title: "Sorry, we're not able to ship more of this product yet",
+              content: "Only " + stock + " items available",
+              animation: 'zoomIn',
+              addClass: 'alert_no_stock',
             });
-            myModal.open();
+            out_of_stock_popup.open();
             return;}
 
         max_value = parseInt(event.target.max) // max quantity value
@@ -66,8 +66,9 @@ $(document).ready(function() {
         var row_number = /cart_row_(.*)/.exec(cart_row_id)[1];
         product_end_user_price = document.getElementById('product_' + row_number + '_end_user_price'); // end price of the current product per piece
         total_end_user_price = document.getElementById('order_line_' + row_number + '_end_user_price'); // end price of the current product per line
+        // product_selling_price = document.getElementById('product_' + row_number + '_selling_price'); // selling price of the current product per piece
 
-        current_order_total_end_user_price = parseInt(0) // current total cart price
+        current_order_total_end_user_price = parseInt(0) // current total cart end user price
         for (total_per_row of $("[id^=order_line_]")) {current_order_total_end_user_price += parseFloat(total_per_row.innerHTML)}
         new_order_total_end_user_price = (current_order_total_end_user_price - parseFloat(total_end_user_price.innerHTML) + parseFloat(product_end_user_price.innerHTML) * current_value).toFixed(2) // total cart price with entered quantity value
 
@@ -76,15 +77,27 @@ $(document).ready(function() {
             new_order_total_end_user_price = (current_order_total_end_user_price - parseFloat(total_end_user_price.innerHTML) + parseFloat(product_end_user_price.innerHTML) * current_value).toFixed(2)
             alert('Total price exceeds 9999.99 $')
         }
-        // update all price sums
+        // update total end user price
         total_end_user_price.innerHTML = (parseFloat(product_end_user_price.innerHTML) * current_value).toFixed(2)
         for (cart_total of $('[id^=cart_price]')) {cart_total.innerHTML = new_order_total_end_user_price}
+
+        // update total selling price
+        new_order_total_selling_price = parseInt(0) // new total cart selling price
+        for (cart_row of $("[id^=cart_row_]")) {
+            var row_number = /cart_row_(.*)/.exec(cart_row.id)[1];
+            quantity = document.getElementById('quantity_' + row_number).value // quantity by row
+            selling_price = document.getElementById('product_' + row_number + '_selling_price') // selling_price by row
+            end_user_price = document.getElementById('product_' + row_number + '_end_user_price') // end_user_price by row
+            if (selling_price) {selling_price = selling_price.innerHTML} // use selling price if the product is discounted
+            else {selling_price = end_user_price.innerHTML} // or end_user_price if not
+            new_order_total_selling_price += parseFloat(selling_price).toFixed(2) * parseInt(quantity)
+        }
+        document.getElementById('cart_selling_price_bottom').innerHTML = new_order_total_selling_price.toFixed(2)
 
         // update items_total
         new_items_total = 0
         for (items_number of $("[id^=quantity_]")) {new_items_total += parseInt(items_number.value)}
         for (items_total of $('#cart_items')) {items_total.innerHTML = new_items_total}
-
     });
 
     $(".remove").click(function(){
