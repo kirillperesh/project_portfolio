0815642bd713256b72e4e880b219b53e71cb84f8
kirill <kirill.fasmer@gmail.com>
2021-08-19 17:44:13 +0300

    update cart page

--- glyke_back/static/glyke_back/js/cart.js
+++ glyke_back/static/glyke_back/js/cart.js
@@ -1,48 +1,4 @@
-// var delete_cart_row = function(cart_row_id) {
-//     var cart_row = document.getElementById(cart_row_id)
-//     cart_row.remove()
-// }
-
-// var check = false;
-
-// function changeVal(el) {
-//   var qt = parseFloat(el.parent().children(".qt").html());
-//   var price = parseFloat(el.parent().children(".price").html());
-//   var eq = Math.round(price * qt * 100) / 100;
-
-//   el.parent().children(".full-price").html( eq + "€" );
-
-//   changeTotal();
-// }
-
-// function changeTotal() {
-
-//   var price = 0;
-
-//   $(".full-price").each(function(index){
-//     price += parseFloat($(".full-price").eq(index).html());
-//   });
-
-//   price = Math.round(price * 100) / 100;
-//   var tax = Math.round(price * 0.05 * 100) / 100
-//   var shipping = parseFloat($(".shipping span").html());
-//   var fullPrice = Math.round((price + tax + shipping) *100) / 100;
-
-//   if(price == 0) {
-//     fullPrice = 0;
-//   }
-
-//   $(".subtotal span").html(price);
-//   $(".tax span").html(tax);
-//   $(".total span").html(fullPrice);
-// }
-
 $(document).ready(function() {
-    // $('#cart_table').DataTable( {
-    //     dom: '',
-    //     "lengthMenu": [ [-1], ["All"]],
-    // } );
-
     $("[id^=quantity_]").change(function (event) { // update total price per position
         current_value = parseInt(event.target.value) // entered quantity value
 
@@ -51,10 +7,10 @@ $(document).ready(function() {
         if (current_value > stock) {
             event.target.value = stock
             var out_of_stock_popup = new jBox('Modal', {
-              title: "Sorry, we're not able to ship more of this product yet",
-              content: "Only " + stock + " items available",
-              animation: 'zoomIn',
-              addClass: 'alert_no_stock',
+                title: "Sorry, we're not able to ship more of this product yet",
+                content: "Only " + stock + " items available",
+                animation: 'zoomIn',
+                addClass: 'alert_no_stock',
             });
             out_of_stock_popup.open();
             return;}
@@ -66,7 +22,6 @@ $(document).ready(function() {
         var row_number = /cart_row_(.*)/.exec(cart_row_id)[1];
         product_end_user_price = document.getElementById('product_' + row_number + '_end_user_price'); // end price of the current product per piece
         total_end_user_price = document.getElementById('order_line_' + row_number + '_end_user_price'); // end price of the current product per line
-        // product_selling_price = document.getElementById('product_' + row_number + '_selling_price'); // selling price of the current product per piece
 
         current_order_total_end_user_price = parseInt(0) // current total cart end user price
         for (total_per_row of $("[id^=order_line_]")) {current_order_total_end_user_price += parseFloat(total_per_row.innerHTML)}
@@ -106,53 +61,32 @@ $(document).ready(function() {
         el.parent().parent().children("footer").children(".qt").children("input").val(0).change()
         el.parent().parent().addClass("removed");
         window.setTimeout(
-          function(){
-            el.parent().parent().slideUp('fast', function() {
-              el.parent().parent().remove();
-              if($(".product").length == 0) {
-                // if(check) {
-                //   $("#cart").html("<h1>The shop does not function, yet!</h1><p>If you liked my shopping cart, please take a second and heart this Pen on <a href='https://codepen.io/ziga-miklic/pen/xhpob'>CodePen</a>. Thank you!</p>");
-                // } else {
-                  $("#cart").html("<h1>No products!</h1>");
-                // }
-              }
-            //   changeTotal();
-            });
-          }, 200);
-      });
+            function(){
+                el.parent().parent().slideUp('fast', function() {
+                    el.parent().parent().remove();
+                    if($(".product").length == 0) {$("#cart").html("<h1>No products yet</h1>");}
+                });
+            }, 200);
+    });
 
-      $(".qt-plus").click(function(){
-        // $(this).parent().children(".qt").children("input").val(parseInt($(this).parent().children(".qt").children("input").val()) + 1);
+    $(".qt-plus").click(function(){
         var el = $(this);
         child = el.parent().children(".qt").children("input");
-
         child.val(parseInt(child.val()) + 1).change();
         el.parent().children(".full-price").addClass("added");
         window.setTimeout(function(){el.parent().children(".full-price").removeClass("added");
-        // changeVal(el)
         ;}, 150);
-      });
+    });
 
-      $(".qt-minus").click(function(){
+    $(".qt-minus").click(function(){
         var el = $(this);
-
         child = el.parent().children(".qt").children("input");
-
         if(parseInt(child.val()) > 1) {child.val(parseInt(child.val()) - 1).change();}
-
         el.parent().children(".full-price").addClass("minused");
         window.setTimeout(function(){el.parent().children(".full-price").removeClass("minused");
-        // changeVal(el);
         }, 150);
-      });
-
-      window.setTimeout(function(){$(".is-open").removeClass("is-open")}, 1200);
-
-    //   $(".btn").click(function(){
-    //     check = true;
-    //     $(".remove").click();
-    //   });
-
+    });
+    window.setTimeout(function(){$(".is-open").removeClass("is-open")}, 1200);
 } );
 
 
