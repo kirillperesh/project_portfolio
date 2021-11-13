80c4ed25afcfd9a3f8648378d507d8f0fa15093a
kirill <kirill.fasmer@gmail.com>
2021-11-13 12:46:37 +0300

    update forms

--- glyke_back/forms.py
+++ glyke_back/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm, UsernameField, PasswordChangeForm
 from django.utils.translation import gettext_lazy as _
 
 from .models import Category, Product
@@ -85,4 +85,18 @@ class CustomPasswordChangeForm(PasswordChangeForm):
         self.fields['new_password1'].widget = forms.PasswordInput(attrs={'class':'required', 'placeholder': 'New password'})
         self.fields['new_password2'].widget = forms.PasswordInput(attrs={'class':'required', 'placeholder': 'Repeat new password'})
 
-# TODO add UserChangeForm
+class CustomUserChangeForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        field_classes = {'username': UsernameField}
+
+    def __init__(self, *args, **kwargs):
+        super(UserChangeForm, self).__init__(*args, **kwargs)
+        # self.fields['old_password'].widget = forms.PasswordInput(attrs={'class':'required', 'placeholder': 'Old password'})
+        # self.fields['new_password1'].widget = forms.PasswordInput(attrs={'class':'required', 'placeholder': 'New password'})
+        # self.fields['new_password2'].widget = forms.PasswordInput(attrs={'class':'required', 'placeholder': 'Repeat new password'})
+
+
